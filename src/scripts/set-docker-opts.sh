47a11c7f6542242621b87f620e0e6ea4c5278e48
Justin Menga <justin.menga@gmail.com>
2017-03-10 13:45:47 +1300

    Set longer Docker service timeout

--- src/scripts/set-docker-opts.sh
+++ src/scripts/set-docker-opts.sh
@@ -2,9 +2,12 @@
 set -e
 
 # Set the Docker bridge interface to CGNAT (RFC 6598) address space
-# Set user namespace remap setting
 subnet=100.64.0.1/24
-remap=default
+
+# Override default startup timeout since we've observed longer start times
+timeout=60
 
 echo "### Setting Docker bridge interface subnet to $subnet ###"
 sudo sed -i -e "s|^\(OPTIONS=\".*\)\"$|\1 --bip $subnet\"|" /etc/sysconfig/docker
+echo "### Setting docker startup timeout to $timeout seconds ###"
+sudo sed -i -e "s/up to 10/up to $timeout/" -e "s/tries -lt 10/tries -lt $timeout/" /etc/init.d/docker
